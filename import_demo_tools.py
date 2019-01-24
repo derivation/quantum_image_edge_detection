6660c400af9df608b4d4054282218dd805eef84e
Paul Kassebaum <paul.kassebaum@ibm.com>
2019-01-24 10:22:58 -0500

    motivate the demo
    and add the whole program at the top

---
+++ import_demo_tools.py
@@ -0,0 +1,7 @@
+# import_demo_tools.py
+
+import numpy as np
+from numpy import pi
+import matplotlib.pyplot as plt
+from qiskit_aqua.components.initial_states import Custom
+from qiskit import BasicAer, execute
